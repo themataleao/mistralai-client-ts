f967ae5a42243374723c07caad1c14f3c23761b7
willson <willhausmarc@gmail.com>
2023-12-17 12:46:39 +0100

    setup auto publish to npm

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloMistral = void 0;
-const helloMistral = () => {
-    console.log("Hello Mistral!");
-};
-exports.helloMistral = helloMistral;
