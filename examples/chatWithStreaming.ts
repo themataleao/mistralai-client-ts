5d3f0270790a83ebbadc05805eca7f61cbdb748e
willson <willhausmarc@gmail.com>
2023-12-18 22:28:54 +0100

    chat with stream example

---
+++ examples/chatWithStreaming.ts
@@ -0,0 +1,17 @@
+import { MistralClient } from "@ai-utils/mistral";
+
+const apiKey = process.env.MISTRAL_API_KEY || "";
+
+const client = new MistralClient(apiKey);
+
+client
+  .chatStream({
+    model: "mistral-tiny",
+    messages: [{ role: "user", content: "What is the best French cheese?" }],
+  })
+  .then((response) => {
+    console.log(response.choices[0]);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
